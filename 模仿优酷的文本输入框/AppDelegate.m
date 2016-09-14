fdab9d1b32eb42dd086b17a9c187b3a23f05b1a2
Xxxda <xiaotengda9@163.com>
2016-09-14 09:19:45 +0800

    new
    test

--- 模仿优酷的文本输入框/AppDelegate.m
+++ 模仿优酷的文本输入框/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "SYFTextBoxController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    SYFTextBoxController  * box = [[SYFTextBoxController alloc] initWithNibName:@"SYFTextBoxController" bundle:nil];
+    self.window.rootViewController = box;
     return YES;
 }
 
